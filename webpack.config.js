ed5437dabda665abe58200dea2d3f3a98b990d22
Muhammet Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2023-05-01 03:25:06 -0400

    Update webpack.config.js
    Signed-off-by: Muhammet Balsoy <28816254+fatihbalsoy@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,9 @@ const config = {
     ],
   },
   resolve: {
+    alias: {
+        three: path.resolve('./node_modules/three')
+    },
     extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
   },
 };
