65759a7ed61b6fad9f12138759133cdfe60e10dd
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2023-09-07 17:45:50 -0400

    Documentation

--- src/scene/camera.ts
+++ src/scene/camera.ts
@@ -43,9 +43,16 @@ class SceneCamera extends THREE.PerspectiveCamera {
         }
     }
 
+    /**
+     * Animates the camera from its current target to the given target.
+     * 
+     * @param object The object for the camera to target
+     * @param duration The time it takes for the animation to play in milliseconds
+     */
     animateLookAt(object: Planet | Star, duration: number) {
         let camera = this
         let nextPlanetCoords = object.position
+        // Convert current planet coordinates to data type accepted by TWEEN
         let currentPlanetCoords = Settings.lookAt.position
         let cameraLookCoords = { x: currentPlanetCoords.x, y: currentPlanetCoords.y, z: currentPlanetCoords.z }
 
@@ -66,20 +73,35 @@ class SceneCamera extends THREE.PerspectiveCamera {
             .start()
     }
 
+    /**
+     * Moves the camera from its current position to a position 
+     * near the given planet. Keeping the planet in its field of view.
+     * 
+     * @param planet The planet in which the camera flies to.
+     * @param duration The time it takes for the animation to play in milliseconds.
+     */
     flyTo(planet: Planet, duration: number) {
         let planetCoords = planet.position
+        // Convert current camera coordinates to data type accepted by TWEEN
         let camera = this
         let cameraCoords = { x: camera.position.x, y: camera.position.y, z: camera.position.z }
 
+        // The amount of space for the planet to cover on the screen 
+        // once the camera reaches its destination.
         var fovScale = .5
         if (planet.id == "saturn") {
-            fovScale = .3
+            fovScale = .3 // This takes saturn's rings into account
         }
         let fovRadians = camera.fov * Math.PI / 180
+        // Calculate the optimal distance between the target planet and the 
+        // camera such that the planet covers the specified area on the screen.
         let distance = Math.sqrt(Math.pow(planet.getRadius() / Math.tan(fovRadians * fovScale / 2), 2) + Math.pow(planet.getRadius(), 2))
+        // The direction vector facing the camera (in its pre-animation state) from the target planet
         let p2cVector = new THREE.Vector3(planetCoords.x - camera.position.x, planetCoords.y - camera.position.y, planetCoords.z - camera.position.z)
 
+        // Calculate the distance from the target planet
         let newCoordsLength = p2cVector.length() - distance
+        // Calculate the camera's new position
         let newCoordsVector = p2cVector.multiplyScalar(newCoordsLength / p2cVector.length())
         let newCoords = new THREE.Vector3(camera.position.x + newCoordsVector.x, camera.position.y + newCoordsVector.y, camera.position.z + newCoordsVector.z)
 
